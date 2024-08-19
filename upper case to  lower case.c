6bc15a06b873e75dd2368d2f7853c3209cad24b5
Md. Maruf Hasan <154753629+0Maruf@users.noreply.github.com>
2024-08-19 10:39:02 +0600

    Create upper case to  lower case.c

---
+++ upper case to  lower case.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (){
+/*
+char lower ;
+printf ("enter lower letter :");
+scanf("%c",& lower );
+printf("The upper case: %c ", lower - 32);
+*/
+//lower to upper letter convert using ascii vslue
+// using built in function
+//lower - 32 using any char upper case fined .
+
+char upper ;
+printf ("enter upper letter :");
+scanf("%c",& upper );
+printf("The Lower  case: %c ",upper + 32);
+
+//lower - 32 using any char lower  case fined .
+
+}
