bd98cc6019478efcdd9e772b6e4f6f6c3fb3d29e
Md. Maruf Hasan <154753629+0Maruf@users.noreply.github.com>
2024-08-19 10:30:03 +0600

    Add files via upload

---
+++ ascii to value fined.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+
+char ch ;
+printf("enter any character :");
+scanf("%c",& ch);
+printf("The ascii value : %d \n", ch);
+
+
+}
---
+++ convert ascii value.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+//check ascii code for any number
+int n;
+printf("Enter any number =");
+scanf("%d",&n);
+printf("The ascii char is:  %c ", n );
+
+return 0;
+
+
+}
