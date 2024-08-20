5bcb5f1085c7519f683e32ba440a952d308d1dc0
Md. Maruf Hasan <154753629+0Maruf@users.noreply.github.com>
2024-08-20 23:59:22 +0600

    Add files via upload

---
+++ even odd using for loop.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main (){
+    int i;
+    for(i=2 ; i<=100;i=i+2)//i=1 using odd number .
+    printf("%d \n",i);
+
+
+
+getch();
+}
---
+++ loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+
+int i;
+for (i=1; i<=6; i++){  //initalization
+printf("Hello World \n");
+printf("%d\n", i);
+
+}
+// for loop, while loop , do while
+getch();
+}
