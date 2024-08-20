c17dd54c2af6033b7e5fc94cee062e3d2b9def8e
Md. Maruf Hasan <154753629+0Maruf@users.noreply.github.com>
2024-08-21 00:31:42 +0600

    Add files via upload

---
+++ triangleArea.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+float base , height , area;
+printf("Enter base : ");
+scanf("%f", & base );
+printf("Enter Height  : ");
+scanf("%f", & height  );
+
+area = 0.5* base * height ;
+printf("%f\n" , area );
+
+return 0;
+}
+
+n
